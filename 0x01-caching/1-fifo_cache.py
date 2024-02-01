2cc164051260e1524241fa894255cce843c3a42d
tituz175 <tobititus01@gmail.com>
2024-01-31 19:35:07 -0700

    task 1 modified

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,16 +13,16 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key and item:
-            if key in self.cache_data.keys():
-                self.cache_data[key] = item
-            elif len(self.cache_data.keys()) < BaseCaching.MAX_ITEMS:
-                self.cache_data[key] = item
-            else:
-                removed_key = list(self.cache_data.keys())[0]
-                self.cache_data.pop(list(self.cache_data.keys())[0])
-                print("DISCARD:{}".format(removed_key))
-                self.cache_data[key] = item
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                remove_key = next(iter(self.cache_data.keys()))
+                del self.cache_data[remove_key]
+                print("DISCARD: {}". format(remove_key))
+
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key
