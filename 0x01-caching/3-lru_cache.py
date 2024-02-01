e3e4b9ef2215f3e44efad7a08f3950ed10a8bb1e
tituz175 <tobititus01@gmail.com>
2024-01-31 20:10:24 -0700

    task 3 modified

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Module Documentation"""
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class LRUCache(BaseCaching):
@@ -10,28 +9,27 @@ class LRUCache(BaseCaching):
     def __init__(self):
         """Constructor documentation"""
         super().__init__()
-        self.order = OrderedDict()
+        self.order = []
 
     def put(self, key, item):
         """ Add an item in the cache
         """
-        if key and item:
-            if key in self.cache_data.keys():
-                del self.order[key]
-            elif len(self.cache_data.keys()) >= BaseCaching.MAX_ITEMS:
-                removed_key, _ = self.order.popitem(last=False)
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if key not in self.order:
+                self.order.append(key)
+            else:
+                self.order.append(self.order.pop(self.order.index(key)))
+            if len(self.order) > BaseCaching.MAX_ITEMS:
+                removed_key = self.order.pop(0)
                 del self.cache_data[removed_key]
                 print("DISCARD:{}".format(removed_key))
-            self.cache_data[key] = item
-            self.order[key] = None
 
     def get(self, key):
         """ Get an item by key
         """
         if key in self.cache_data.keys():
-            # Move the key to the end of the ordered dict to mark it as recently used
-            del self.order[key]
-            self.order[key] = None
+            self.order.append(self.order.pop(self.order.index(key)))
             return self.cache_data[key]
         else:
             return None
