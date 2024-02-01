b71eab77fd374a7e87574bb66f681b97ced865f8
tituz175 <tobititus01@gmail.com>
2024-01-31 19:46:25 -0700

    task 2 modified

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -20,7 +20,7 @@ class LIFOCache(BaseCaching):
                     and key not in self.cache_data.keys():
                 removed_key, _ = self.cache_data.popitem()
                 del self.cache_data[removed_key]
-                print("DISCARD:{}".format(removed_key))
+                print("DISCARD: {}". format(removed_key))
 
             self.cache_data[key] = item
 
